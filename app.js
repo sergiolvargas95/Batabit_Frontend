1f40266a9cde836f0c341a07b2b4ec2ed37969c7
sergiolvargas95 <65960993+sergiolvargas95@users.noreply.github.com>
2021-01-30 17:55:49 -0500

    Se le agregó un botón de dark mode con javascript y css

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ moveToNextSlide = () => {
     updateSlidePosition();
 }
 
-const contenido = document.querySelector('#tabla_contenido')
+const contenido = document.querySelector('#currency-table--container')
 const UpDow = document.querySelector('#direction')
 
 const opcionesCriptomonedas = async() => {
@@ -55,23 +55,48 @@ const opcionesCriptomonedas = async() => {
 
 tabla = resultado => {
     console.log(resultado)
-    contenido.innerHTML = ''
-
+    //contenido.innerHTML = ''
     resultado.Data.map(mostrar => {
-        if (mostrar.DISPLAY.USD.CHANGE24HOUR < mostrar.DISPLAY.USD.CHANGEDAY) {
-            UpDow.classList.add('dow')
-        } else {
-            UpDow.classList.add('up')
-        }
         contenido.innerHTML += `
-        <tr>
-            <td class="table__top-left">${mostrar.CoinInfo.FullName}</td>
-            <td class="table__top-right table__right">${mostrar.DISPLAY.USD.CHANGE24HOUR} <span id="direction"></span></td>
-        </tr>
+        <div id="tabla_contenido">
+            <div class="currency-name">${mostrar.CoinInfo.FullName}</div>
+        </div>
+            <div id="tabla_precio">
+        <div class="currency-price">$ ${mostrar.DISPLAY.USD.CHANGEPCT24HOUR}</span></div>
         `
-    });
+        }
+    );
 }
 
 opcionesCriptomonedas()
 
+const btnSwitch = document.querySelector('#switch')
+const mainExchangeContainer = document.querySelector('#main-exchange-container')
+const mainProductDetail = document.querySelector('#main-product-detail')
+const exchangeTitle = document.querySelector('.main-exchange-container--title1')
+const exchangeSubtitle = document.querySelector('.main-exchange-container--subtitle')
+const productTitle = document.querySelector('.product-detail--title1')
+const productSubtitle = document.querySelector('.product-detail--subtitle')
+const mainPlansContainer = document.querySelector('.main-plans-container')
+const mainPlansContainerTitle = document.querySelector('.plans--title1')
+const mainPlansContainerSubitle = document.querySelector('.plans--subtitle')
+const buttonNext = document.querySelector('.carousel__button--next')
+const buttonPrev = document.querySelector('.carousel__button--prev')
 
+
+
+btnSwitch.addEventListener('click', () => {
+    mainExchangeContainer.classList.toggle('dark');
+    mainProductDetail.classList.toggle('dark');
+    exchangeTitle.classList.toggle('dark-letters');
+    exchangeSubtitle.classList.toggle('dark-letters');
+    productTitle.classList.toggle('dark-letters');
+    productSubtitle.classList.toggle('dark-letters');
+    mainPlansContainer.classList.toggle('dark');
+    contenido.classList.toggle('dark-table');
+    mainPlansContainerTitle.classList.toggle('dark-letters')
+    mainPlansContainerSubitle.classList.toggle('dark-letters')
+    buttonNext.classList.toggle('carousel__button--dark')
+    buttonPrev.classList.toggle('carousel__button--dark')
+    btnSwitch.classList.toggle('active');
+});
